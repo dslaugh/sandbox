89fe80821bcf4aaef1c3609a71fea67936804296
David Slaugh <dslaugh@gmail.com>
2016-08-25 08:37:06 -0600

    maybe monad

---
+++ maybe_monad/index.js
@@ -0,0 +1,146 @@
+// http://jrsinclair.com/articles/2016/marvellously-mysterious-javascript-maybe-monad/
+
+var user = {
+	email: 'james@example.com',
+	accountDetails: {
+		address: {
+			street: '123 Fake St',
+			city: 'Exampleville',
+			province: 'NS',
+			postcode: '1234'
+		}
+	},
+	preferences: {}
+};
+
+// var user = {};
+
+// var user = null;
+
+// var user = {
+// 	email: 'james@example.com',
+// 	accountDetails: {}
+// };
+
+var banners = {
+	'AB': '/assets/banners/alberta.jpg',
+	'BC': '/assets/banners/british-columbia.jpg',
+	'MB': '/assets/banners/manitoba.jpg',
+	'NL': '/assets/banners/newfoundland-labrador.jpg',
+	'NS': '/assets/banners/northwest-territories.jpg',
+	'ON': '/assets/banners/ontario.jpg',
+	'PE': '/assets/banners/prince-edward.jpg',
+	'QC': '/assets/banners/quebec.jpg',
+	'SE': '/assets/banners/saskatchewan.jpg',
+	'YT': '/assets/banners/yukon.jpg'
+};
+
+// function getUserBanner(banners, user) {
+// 	return banners[user.accountDetails.address.province];
+// }
+
+// function getUserBanner(banners, user) {
+// 	if (user.accountDetails !== undefined) {
+// 		return banners[user.accountDetails.address.province];
+// 	}
+// }
+
+// function getUserBanner(banners, user) {
+// 	if (user !== null) {
+// 		if (user.accountDetails !== undefined) {
+// 			return banners[user.accountDetails.address.province];
+// 		}
+// 	}
+// }
+
+// function getUserBanner(banners, user) {
+// 	if (user !== null) {
+// 		if (user.accountDetails !== undefined) {
+// 			if (user.accountDetails.address !== undefined) {
+// 				return banners[user.accountDetails.address.province];
+// 			}
+// 		}
+// 	}
+// }
+
+var Maybe = function(val) {
+	this.__value = val;
+};
+Maybe.of = function(val) {
+	return new Maybe(val);
+};
+Maybe.prototype.isNothing = function() {
+	return (this.__value === null || this.__value === undefined);
+};
+Maybe.prototype.map = function(f) {
+	if (this.isNothing()) {
+		return Maybe.of(null);
+	}
+	return Maybe.of(f(this.__value));
+};
+Maybe.prototype.join = function() {
+	return this.__value;
+};
+Maybe.prototype.chain = function(f) {
+	return this.map(f).join();
+};
+Maybe.prototype.orElse = function(defaultVal) {
+	if (this.isNothing()) {
+		return Maybe.of(defaultVal);
+	}
+	return this;
+};
+
+function prop(prop, obj) {
+	if (obj === undefined) {
+		return function(obj) {
+			return obj[prop];
+		}
+	} else {
+		return obj[prop];
+	}
+}
+
+function getProvinceBanner(province) {
+	return Maybe.of(banners[province]);
+}
+
+function getUserBanner(banners, user) {
+	return Maybe.of(user)
+		.map(prop('accountDetails'))
+		.map(prop('address'))
+		.map(prop('province'))
+		.map(getProvinceBanner)
+		.orElse('/assets/banners/default.jpg')
+}
+
+// function getUserBanner(banners, user) {
+// 	return Maybe.of(user)
+// 		.map(prop('accountDetails'))
+// 		.map(prop('address'))
+// 		.map(prop('province'))
+// 		.chain(getProvinceBanner);
+// }
+
+var bannerSrc = getUserBanner(banners, user);
+
+// Faking DOM
+var document = {
+	querySelector: function(selector) {
+		return {class: selector, src: undefined}
+	}
+};
+
+var el = document.querySelector('.banner');
+
+el.src = bannerSrc;
+
+console.log('el', el);
+
+
+
+
+
+
+
+
